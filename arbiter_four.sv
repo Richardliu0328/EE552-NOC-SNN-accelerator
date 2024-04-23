920342f00d3220d284a7ccad93e3a217d4aef982
James_Lin <91178610+JamesLinnnnn@users.noreply.github.com>
2024-04-22 22:09:04 -0700

    Update arbiter_four.sv

--- arbiter_four.sv
+++ arbiter_four.sv
@@ -9,5 +9,4 @@ Channel #(.WIDTH(32),.hsProtocol(P4PhaseBD))  out1(),out2(),out3();
 arbiter_two ar0(in0, in1, out1);
 arbiter_two ar1(in2,in3,out2);
 arbiter_two ar3(out1, out2,out);
-buffer buff(out3,out);
 endmodule
